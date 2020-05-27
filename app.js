2e20d5046f0c7334487a054d32d562fafb294198
Frédéric Le Coquil <flecoqui@microsoft.com>
2020-05-27 08:15:49 +0200

    update error view

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{ message: err.message });
 });
 console.log('#########################################Starting-env: ', process.env);
 module.exports = app;
